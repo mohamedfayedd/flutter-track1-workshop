3040bc440d737f3aa83326d5f0d991f5afe32135
Ussifjo <ye0538649@gmail.com>
2024-09-08 16:26:40 +0300

    handel xo colors

---
+++ xo_app/lib/core/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color white= Colors.white;
+  static Color Pr = const Color(0xffbdb18c);
+  static Color ag= const Color(0xff5d9e9f);
+  static Color ne= const Color(0xff0b565a);
+  static Color ff= const Color(0xff17858b);
+}
