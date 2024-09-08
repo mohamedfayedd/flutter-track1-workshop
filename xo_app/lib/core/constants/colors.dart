2381a504dbe7c422f2d88af11e14611e3b674b1d
Ussifjo <ye0538649@gmail.com>
2024-09-08 16:45:58 +0300

    handle xo colors

--- xo_app/lib/core/constants/colors.dart
+++ xo_app/lib/core/constants/colors.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static Color white= Colors.white;
-  static Color Pr = const Color(0xffbdb18c);
-  static Color ag= const Color(0xff5d9e9f);
-  static Color ne= const Color(0xff0b565a);
-  static Color ff= const Color(0xff17858b);
+  static Color primary = const Color(0xffbdb18c);
+  static Color secondary= const Color(0xff5d9e9f);
+  static Color third= const Color(0xff0b565a);
+  static Color fourth= const Color(0xff17858b);
 }
